40fe4665ee4a6d760794f44551e4e93c8b7d1317
Peter Rose <peter.rose56@gmail.com>
2020-12-01 20:21:13 -0500

    Updated __init__ file to match changes to params_converter

--- ufss/HLG/__init__.py
+++ ufss/HLG/__init__.py
@@ -1,7 +1,7 @@
 from .eigen_generator import EigenGenerator
 from .simple_eigen_generator import SimpleEigenGenerator
 from .dipole_operator import DipoleConverter
-from .params_converter import convert, convert_open
+from .params_converter import convert
 from .dipole_operator import CalculateCartesianDipoleOperator
 from .DiabaticLindblad_Liouvillians import OpenPolymer, OpenPolymerVibrations
 from .Hamiltonians import PolymerVibrations,DiagonalizeHamiltonian
