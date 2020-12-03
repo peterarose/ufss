fe20c92c59917efba6c3029a76334b06c10a01bc
Peter Rose <peter.rose56@gmail.com>
2020-12-03 09:39:33 -0500

    Fixed __init__ file to import RKE as well as UF2

--- ufss/__init__.py
+++ ufss/__init__.py
@@ -1,9 +1,8 @@
 from ufss.diagram_automation import DiagramGenerator
-# from ufss.heaviside_convolve import HeavisideConvolve
 from ufss.UF2 import Wavepackets
-# from ufss.RKE import RKE_Wavepackets
+from ufss.RKE import RKE_Wavepackets
 from ufss.UF2 import DensityMatrices
-# from ufss.RKE import RKE_DensityMatrices
+from ufss.RKE import RKE_DensityMatrices
 from ufss.dipole_pruning import DipolePruning
 import ufss.signals as signals
 import ufss.HLG as HLG
