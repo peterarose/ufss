1d12594506791281ac7425a9de6b7e7b5ef43134
Peter Rose <peter.rose56@gmail.com>
2021-03-10 11:19:50 -0500

    Updated params converter to work with anharmonic vibrational mode inputs

--- ufss/HLG/params_converter.py
+++ ufss/HLG/params_converter.py
@@ -5,45 +5,107 @@ import copy
 import shutil
 import math
 
-def convert_simple_dict(vib_dict):
-    site = vib_dict['site_label']
+# def convert_simple_dict(vib_dict,*,halfway_basis=True):
+#     new_vib_dict = copy.deepcopy(vib_dict)
+#     d = vib_dict['displacement']
+#     try:
+#         alpha = vib_dict['alpha']
+#     except KeyError:
+#         alpha = 1
+#     p0 = [1]
+#     p1 = [alpha**2]
+#     k0 = [1]
+#     k1 = [1]
+#     try:
+#         l = vib_dict['reorganization']
+#     except KeyError:
+#         l = -alpha**2 * d**2
+
+#     new_d = d*alpha**(1/4)/2
+    
+#     if halfway_basis:
+#         k_modifier = [alpha**(i/4) for i in range(2,len(k0)+2)]
+#         p_modifier = [alpha**(-i/4) for i in range(2,len(p0)+2)]
+#     else:
+#         k_modifier = [1 for i in range(2,len(k0)+2)]
+#         p_modifier = [1 for i in range(2,len(p0)+2)]
+        
+#     new_k0 = [a*b for a,b in zip(k0,k_modifier)]
+#     new_k1 = [a*b for a,b in zip(k1,k_modifier)]
+    
+#     new_p0 = [a*b for a,b in zip(p0,p_modifier)]
+#     new_p1 = [a*b for a,b in zip(p1,p_modifier)]
+
+#     new_vib_dict['reorganization'] = [0,l]
+#     new_vib_dict['kinetic'] = [new_k0,new_k1]
+#     new_vib_dict['potential'] = [new_p0,new_p1]
+
+#     if halfway_basis:
+#         new_vib_dict['displacement'] = [-new_d,new_d]
+#     else:
+#         new_vib_dict['displacement'] = [0,d]
+
+#     return new_vib_dict
+
+def convert_general_dict(vib_dict,*,halfway_basis=True):
+    new_vib_dict = copy.deepcopy(vib_dict)
     d = vib_dict['displacement']
+    
+    if 'alpha' in vib_dict.keys() and 'potential' in vib_dict.keys():
+        raise Exception('Cannot specify potential and alpha simultaneously')
     try:
-        alpha = vib_dict['alpha']
+        p0,p1 = vib_dict['potential']
+        alpha = (p1[0]/p0[0])**(1/2)
     except KeyError:
-        alpha = 1
-    p0 = [1]
-    p1 = [alpha**2]
-    k0 = [1]
-    k1 = [1]
+        p0 = [1]
+        p1 = [1]
+        try:
+            alpha = vib_dict['alpha']
+            new_vib_dict.pop('alpha')
+        except KeyError:
+            alpha = 1
+    try:
+        k0,k1 = vib_dict['kinetic']
+    except KeyError:
+        k0 = [1]
+        k1 = [1]
+    
+    new_d = d*alpha**(1/4)/2
+    
     try:
         l = vib_dict['reorganization']
     except KeyError:
         l = -alpha**2 * d**2
-    
-    k_modifier = [alpha**(i/4) for i in range(2,len(k0)+2)]
-    new_k0 = [a*b for a,b in zip(k0,k_modifier)]
-    new_k1 = [a*b for a,b in zip(k1,k_modifier)]
-    
-    p_modifier = [alpha**(-i/4) for i in range(2,len(p0)+2)]
-    new_p0 = [a*b for a,b in zip(p0,p_modifier)]
-    new_p1 = [a*b for a,b in zip(p1,p_modifier)]
-    
-    new_d = d*alpha**(1/4)/2
 
-    potential1 = new_p1
-    kinetic1 = new_k1
+    if halfway_basis:
+        k0_modifier = [alpha**(i/4) for i in range(2,len(k0)+2)]
+        k1_modifier = [alpha**(i/4) for i in range(2,len(k1)+2)]
+        p0_modifier = [alpha**(-i/4) for i in range(2,len(p0)+2)]
+        p1_modifier = [alpha**(-i/4) for i in range(2,len(p1)+2)]
+    else:
+        k0_modifier = [1 for i in range(2,len(k0)+2)]
+        k1_modifier = [1 for i in range(2,len(k1)+2)]
+        p0_modifier = [1 for i in range(2,len(p0)+2)]
+        p1_modifier = [1 for i in range(2,len(p1)+2)]
+        
+    new_k0 = [a*b for a,b in zip(k0,k0_modifier)]
+    new_k1 = [a*b for a,b in zip(k1,k1_modifier)]
+    
+    new_p0 = [a*b for a,b in zip(p0,p0_modifier)]
+    new_p1 = [a*b for a,b in zip(p1,p1_modifier)]
 
-    new_vib_dict = {'displacement':[-new_d,new_d],'reorganization':[0,l],
-                    'omega_g':vib_dict['omega_g'],'kinetic':[new_k0,new_k1],
-                    'potential':[new_p0,new_p1],'site_label':site}
-    try:
-        new_vib_dict['condon_violation'] = vib_dict['condon_violation']
-    except KeyError:
-        pass
+    new_vib_dict['reorganization'] = [0,l]
+    new_vib_dict['kinetic'] = [new_k0,new_k1]
+    new_vib_dict['potential'] = [new_p0,new_p1]
+
+    if halfway_basis:
+        new_vib_dict['displacement'] = [-new_d,new_d]
+    else:
+        new_vib_dict['displacement'] = [0,d]
+        
     return new_vib_dict
 
-def convert(base_path,*,convert_type='simple'):
+def convert(base_path,*,halfway_basis=True):
     simple_file_name = os.path.join(base_path,'simple_params.yaml')
     parameter_file_name = os.path.join(base_path,'params.yaml')
     with open(simple_file_name,'r') as yamlstream:
@@ -71,10 +133,8 @@ def convert(base_path,*,convert_type='simple'):
         pass
 
     simple_vibrations = simple['vibrations']
-    n = len(simple['site_energies'])
-    
-    if convert_type == 'simple':
-        vibrations = [convert_simple_dict(vib) for vib in simple_vibrations]
+
+    vibrations = [convert_general_dict(vib,halfway_basis=halfway_basis) for vib in simple_vibrations]
         
     params['vibrations'] = vibrations
 
