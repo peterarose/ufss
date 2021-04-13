25ceb557fa1164a20a3fadc24c9aa03cfd9a47be
Peter Rose <peter.rose56@gmail.com>
2021-04-13 15:39:57 -0400

    updated after moving Lindblad constructor and Liouvillian constructor into the general_Liouvillian_classes filee

--- ufss/HLG/Redfield_Liouvillians.py
+++ ufss/HLG/Redfield_Liouvillians.py
@@ -14,8 +14,9 @@ import warnings
 import os
 import time
 
-from .Hamiltonians import DisplacedAnharmonicOscillator, PolymerVibrations, Polymer, DiagonalizeHamiltonian, LadderOperators
-from .params_converter import convert
+from .Hamiltonians import PolymerVibrations
+
+from .general_Liouvillian_classes import LiouvillianConstructor
 
 class OhmicSpectralDensity:
     """Creates an ohmic spectral density function
@@ -1073,68 +1074,3 @@ class DiagonalizeLiouvillian:
             np.savez(os.path.join(self.save_path,'mu_boolean.npz'),**self.boolean_mu)
         else:
             np.savez(os.path.join(self.save_path,'mu.npz'),**self.transformed_mu)
-
-            
-class LindbladConstructor:
-
-    @staticmethod
-    def make_Lindblad_instructions(gamma,O):
-        """O must be square
-"""
-        II = np.eye(O.shape[0])
-        Od = np.conjugate(O.T)
-        leftright = gamma * (-np.dot(Od,O)/2)
-        return [(gamma*O,Od),(leftright,II),(II,leftright)]
-
-    @staticmethod
-    def make_Lindblad_instructions2(gamma,Oket,Obra):
-        IIket = np.eye(Oket.shape[0])
-        IIbra = np.eye(Obra.shape[0])
-        Oketd = np.conjugate(Oket.T)
-        Obrad = np.conjugate(Obra.T)
-        left = gamma * (-np.dot(Oketd,Oket)/2)
-        right = gamma * (-np.dot(Obrad,Obra)/2)
-        return [(gamma*Oket,Obrad),(left,IIbra),(IIket,right)]
-
-    @staticmethod
-    def make_Lindblad_instructions2_Obra0(gamma,Oket,Obra):
-        IIbra = np.eye(Obra.shape[0])
-        Oketd = np.conjugate(Oket.T)
-        left = gamma * (-np.dot(Oketd,Oket)/2)
-        return [(left,IIbra)]
-
-    @staticmethod
-    def make_Lindblad_instructions2_Oket0(gamma,Oket,Obra):
-        IIket = np.eye(Oket.shape[0])
-        Obrad = np.conjugate(Obra.T)
-        right = gamma * (-np.dot(Obrad,Obra)/2)
-        return [(IIket,right)]
-
-class LiouvillianConstructor(LindbladConstructor):
-
-    @staticmethod
-    def make_commutator_instructions(O):
-        """O must be square
-"""
-        II = np.eye(O.shape[0])
-        return [(O,II),(II,-O)]
-
-    @staticmethod
-    def make_commutator_instructions2(Oket,Obra):
-        """
-"""
-        IIket = np.eye(Oket.shape[0])
-        IIbra = np.eye(Obra.shape[0])
-        return [(Oket,IIbra),(IIket,-Obra)]
-
-    @staticmethod
-    def make_Liouvillian(instruction_list,*,sparse=False):
-        if sparse:
-            kron = sp.kron
-        else:
-            kron = np.kron
-        left, right = instruction_list[0]
-        L = kron(left,right.T)
-        for left,right in instruction_list[1:]:
-            L = L + kron(left,right.T)
-        return L
