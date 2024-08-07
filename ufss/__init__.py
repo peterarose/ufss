e522c779992a1ddd31f06a445dbe77ebc6ee06c9
peterarose <peter.rose56@gmail.com>
2024-08-06 22:44:46 -0400

    Modified primary init file to work with restructured code

--- ufss/__init__.py
+++ ufss/__init__.py
@@ -1,10 +1,15 @@
 from ufss.diagram_automation import DiagramGenerator
-from ufss.UF2 import Wavepackets
-from ufss.RKE import RKE_Wavepackets
-from ufss.UF2 import DensityMatrices
-from ufss.RKE import RKE_DensityMatrices
-from ufss.dipole_pruning import DipolePruning
+from ufss.composite_diagrams import CompositeDiagrams
+#from ufss.UF2 import Wavepackets
+#from ufss.UF2 import DensityMatrices
+#from ufss.RKE import RKE_DensityMatrices
+from ufss.signals import UF2Wavefunctions, UF2DensityMatrices, RKWavefunctions, RKDensityMatrices
 import ufss.signals as signals
 import ufss.HLG as HLG
 from ufss.efield_shapes import *
-from ufss.convergence_tester import Convergence, efieldConvergence
+from ufss.convergence_tester import efieldConvergence, Convergence
+
+## Backwards compatibility
+Wavepackets = UF2Wavefunctions
+DensityMatrices = UF2DensityMatrices
+RKE_DensityMatrices = RKDensityMatrices
